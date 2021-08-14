5a72849cea82cf4305e1edff755c720e5b9243a0
rafaelmatostj <rafaelmatostj@gmail.com>
2021-08-14 18:39:01 -0300

    Adc correção de erro no woocommercer com jetpack

---
+++ 5-arithmetic-average.py
@@ -0,0 +1,4 @@
+n1 = float(input('Primeira Nota do aluno: '))
+n2 = float(input('Segunda nota do aluno: '))
+m = (n1 + n2) / 2
+print('A media entre {:.1f} e {:.1f} é igual a {:.1f}' .format(n1, n2, m))
